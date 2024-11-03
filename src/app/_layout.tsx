9aeba07e0a8f4977919724894c6da4e67a0c4ed9
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-11-03 16:33:34 +0530

    welcome page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,32 @@
 import "../global.css";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+// Prevent the splash screen from hiding until the fonts are loaded
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    "DMSans-Bold": require("../assets/fonts/DMSans-Bold.ttf"),
+    "DMSans-Medium": require("../assets/fonts/DMSans-Medium.ttf"),
+    "DMSans-Regular": require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    const hideSplashScreen = async () => {
+      if (fontsLoaded) {
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    hideSplashScreen();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null; // Optionally, render a loading indicator
+  }
+
   return <Stack />;
 }
